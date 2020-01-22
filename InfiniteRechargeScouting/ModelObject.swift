5494abc5c4f78d95ef3395e182b11a89ffb77615
Wesley Vong <wv@vongs.com>
2020-01-21 17:17:54 -0800

    Changed Model Object

---
+++ InfiniteRechargeScouting/ModelObject.swift
@@ -0,0 +1,74 @@
+//
+//  ModelObject.swift
+//  FRCScouting
+//
+//  Created by Wesley Vong on 3/10/19.
+//  Copyright © 2019 Takahashi, Alex. All rights reserved.
+//
+
+import Foundation
+
+class ModelObject {
+    
+    static private(set) var shared = ModelObject()
+    
+    private init() { }
+    
+    func reset() {
+        ModelObject.shared = ModelObject()
+    }
+    // Pre-Match
+    var scoutName = ""
+    var teamName = ""
+    var match = 0
+    var preload = 0
+    var initPos = 0
+    
+    // Auton
+    var autonHigh = 0
+    var autonLow = 0
+    var moved = false
+    var pickup = false
+    var additionalPoints = false
+    
+    // Teleop
+    var teleHitHigh = 0
+    var teleHitLow = 0
+    var teleMissHigh = 0
+    var teleMissLow = 0
+    
+    // Post-Match
+    var disconnect = false
+    var fall = false
+    var trench = false
+    
+    // Color Wheel Select
+    var messedUp = false;
+    var color = false;
+    var rotation = false;
+    var noneColorWheel = false;
+    
+    // Shot Select
+    var nearShot = false
+    var farShot = false;
+    
+    // Hang
+    var hang = false;
+    var balanced = false;
+    var failed = false;
+    var parked = false;
+    var outOfZone = false;
+    
+    var driverPerformance = 0 // 0 = meh, 1 = average, 2 = great
+    
+    var penalties;
+
+}
+
+// Helper to convert Ints to bool
+// Where false is 0 and 1 is true
+public extension Int {
+    public init(_ bool: Bool) {
+        self = bool ? 1 : 0
+    }
+}
